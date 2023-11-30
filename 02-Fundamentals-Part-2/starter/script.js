64ea2f30db8250ba0cf626dac8267a341e02d393
liadA <liad90000@gmail.com>
2023-12-01 00:25:45 +0200

    learning

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//Coding Challange #1
 function codingChallenge1() {
     const calcAverage = (score1, score2, score3) => {
         return (score1 + score2 + score3) / 3
@@ -24,5 +25,20 @@ function codingChallenge1() {
 }
 
 codingChallenge1();
+//################//
 
+
+//Coding Challange #2
+function codingChallenge2() {
+    function calcTip(bill) {
+        let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+        return tip;
+    }
+    const bills = [125, 555, 44];
+    const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+    const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+}
+
+codingChallenge2();
+//################//
 
