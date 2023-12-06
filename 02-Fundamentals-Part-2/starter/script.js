63d19167cdbb9abb9772e327c2caccd46dc2b6a7
liadA <liad90000@gmail.com>
2023-12-06 19:01:31 +0200

    end of section 3 fundamentals part 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -76,3 +76,31 @@ function codingChallenge3() {
 codingChallenge3();
 //################//
 
+//Coding Challange #4
+function codingChalenge4() {
+    const calcTip = function (bill) {
+        return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+    }
+
+    const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+    const tips = [];
+    const totals = [];
+
+    for (let i = 0; i < bills.length; i++) {
+        tips[i] = calcTip(bills[i])
+        totals[i] = tips[i] + bills[i]
+    }
+
+    const calcAverage = arr => {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    };
+
+    calcAverage(totals);
+}
+codingChalenge4();
+//################//
+
