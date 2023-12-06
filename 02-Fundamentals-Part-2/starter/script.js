68c7d782bf4a274f903070d114fb08595d112f2a
LiadArama <96491486+LiadArama@users.noreply.github.com>
2023-12-06 17:22:16 +0200

    learning

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -42,3 +42,37 @@ function codingChallenge2() {
 codingChallenge2();
 //################//
 
+//Coding Cahllenge #3
+function codingChallenge3() {
+    const mark = {
+        mass: 78,
+        height: 1.69,
+        fullName: "Mark Miller",
+        calcBMI: function () {
+            this.bmi = this.mass / (this.height * this.height);
+            return this.bmi;
+        }
+    }
+
+    const john = {
+        mass: 92,
+        height: 1.95,
+        fullName: "John Smith",
+        calcBMI: function () {
+            this.bmi = this.mass / (this.height * this.height);
+            return this.bmi;
+        }
+    }
+
+    mark.calcBMI(); john.calcBMI();
+
+    if (mark.bmi > john.bmi) {
+        console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
+    } else {
+        console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+    }
+}
+
+codingChallenge3();
+//################//
+
