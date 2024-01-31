4d5f5ebafaec243264c5f6d84e9bcfdade634ca9
liadA <liad90000@gmail.com>
2024-01-31 20:42:51 +0200

    section 10 coding challenge

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -32,3 +32,14 @@ poll.displayResults.call({ answer: [5, 2, 3] }); // this works becauase the firs
 poll.displayResults.call({ answer: [1, 5, 3, 9, 6, 1] }, 'string'); // this as well, and then the string 'string' is the second argument in the call method which is the first argument in the displayResults method.
 // [5, 2, 3]
 // [1, 5, 3, 9, 6, 1]
+
+/*****************************************************************************/
+
+// Coding Challenge #2
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
