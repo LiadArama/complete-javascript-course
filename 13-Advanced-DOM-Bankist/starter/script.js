22bc30f67b27d367d12aae918013f6176037c7d8
liadA <liad90000@gmail.com>
2024-05-05 21:49:16 +0300

    learning

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -30,34 +30,126 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (event) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(e.targer.getBoundingClientRect());
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (event) {
+  alert('addEventListener: Great! You are reading the heading :D');
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
 ///////////////////////////////////////
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-const msg = document.createElement('div');
-msg.classList.add('cookie-message'); // adding class to the element
-msg.textContent = 'We use cookies for improved functionality and analytics.';
-msg.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// const msg = document.createElement('div');
+// msg.classList.add('cookie-message'); // adding class to the element
+// msg.textContent = 'We use cookies for improved functionality and analytics.';
+// msg.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-header.append(msg);
+// header.append(msg);
 
 // header.append(msg.cloneNode(true));
 
 // header.before(msg);
 // header.after(msg);
 
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    msg.parentElement.removeChild(msg);
-  });
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     msg.parentElement.removeChild(msg);
+//   });
+
+// msg.style.backgroundColor = '#37383d';
+// msg.style.width = '120%';
+// console.log(msg.style.backgroundColor);
+// // Output: rgb(55, 56, 61)
+
+// console.log(msg.height);
+// // Output: ''
+
+// console.log(getComputedStyle(msg).height);
+
+// msg.style.height =
+//   Number.parseFloat(getComputedStyle(msg).height, 10) + 40 + 'px';
+
+// //Since the CSS variable are decalred under the root, we would need to use the documentElement to access the root element
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
+
+// // Non-standard
+// console.log(logo.designer);
+// // Output: undefined
+// console.log(logo.getAttribute('designer'));
+// // Output: Jonas
+
+// logo.alt = 'Beautiful minimalist logo';
+
+// logo.setAttribute('company', 'Bankist');
+
+// console.log(logo.src);
+// // Output: http://127.0.0.1:8000/img/logo.png
+
+// console.log(logo.getAttribute('src'));
+// // Output: img/logo.png
+
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// // Output: http://127.0.0.1:8000/#
+
+// console.log(link.getAttribute('href'));
+// // Output: #
+
+// console.log(logo.dataset.versionNumber);
+// // Output: 3.0
+
+// logo.classList.add();
+// logo.classList.remove();
+// logo.classList.toggle();
+// logo.classList.contains();
+
+// logo.className = 'jonas'; // Don't use this method
+
+// window.scrollTo(
+//   s1coords.left + window.pageXOffset,
+//   s1coords.top + window.pageYOffset
+// );
+
+// console.log(window.pageXOffset, window.pageYOffset);
+// console.log(
+//   document.documentElement.clientHeight,
+//   document.documentElement.clientWidth
+// );
+
+// window.scrollTo({
+//   left: s1coords.left + window.pageXOffset,
+//   top: s1coords.top + window.pageYOffset,
+//   behavior: 'smooth',
+// });
