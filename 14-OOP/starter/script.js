9a532eb93fe08149ac1c6cd49c074b9134bbdd16
liadA <liad90000@gmail.com>
2024-06-26 22:16:57 +0300

    learning

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -61,30 +61,76 @@ class Person {
   set setFirstName(firstName) {
     this.firstName = firstName;
   }
+
+  static hey() {
+    console.log('hi');
+  }
 }
 
-const account = {
-  owner: 'jonas',
-  movements: [200, 530, 120, 300],
+// const account = {
+//   owner: 'jonas',
+//   movements: [200, 530, 120, 300],
 
-  get latest() {
-    return this.movements.slice(-1).pop();
+//   get latest() {
+//     return this.movements.slice(-1).pop();
+//   },
+
+//   set latest(movemnet) {
+//     this.movements.push(movemnet);
+//   },
+// };
+// console.log(account.latest);
+// // Output: 300
+
+// account.latest = 50;
+// console.log(account.movements);
+// // Output: [200, 530, 120, 300, 50]
+
+// const person = new Person('jonas', 2020);
+// person.age;
+// // Output: 17
+// person.setFirstName = 'new jonas name';
+// console.log(person.firstName);
+// // Output: new jonas name
+
+// Array.from(document.querySelectorAll('h1'));
+// // Output: [h1 (Node)]
+
+// [1, 2, 3].from();
+// // Output: Uncaught TypeError: [1,2,3].from is not a function
+
+// Number.parseFloat(12);
+
+// Person.hi = function () {
+//   console.log('Hey there');
+// };
+
+const Person = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
   },
 
-  set latest(movemnet) {
-    this.movements.push(movemnet);
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
   },
 };
-console.log(account.latest);
-// Output: 300
 
-account.latest = 50;
-console.log(account.movements);
-// Output: [200, 530, 120, 300, 50]
+const steven = Object.create(Person);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+// Output: 35
 
-const person = new Person('jonas', 2020);
-person.age;
-// Output: 17
-person.setFirstName = 'new jonas name';
-console.log(person.firstName);
-// Output: new jonas name
+console.log(steven.__proto__);
+// Output: {calcAge: ƒ}
+
+console.log(steven);
+// Output: {name: "Steven", birthYear: 2002, proto: {calcAge: ƒ}}
+
+const sarah = Object.create(Person);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
+// Output: 58
+console.log(sarah);
+// Output: {firstName: "Sarah", birthYear: 1979, proto: {calcAge: ƒ}}
